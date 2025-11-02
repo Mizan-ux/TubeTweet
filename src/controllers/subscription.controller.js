5c46cb4278dd934230d42f9384df65083b7b0c86
Mizan-ux <mizanmulani1234@gmail.com>
2025-11-02 22:15:48 +0530

    feat: implement toggle subscription functionality with error handling and response messages

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -7,8 +7,53 @@ import { asyncHandler } from "../utils/asyncHandler.js"
 
 
 const toggleSubscription = asyncHandler(async (req, res) => {
-    const { channelId } = req.params
+    const { channelId } = req.params;
     // TODO: toggle subscription
+
+    if (!isValidObjectId(channelId)) {
+        throw new ApiError(400, "Invalid Credentials");
+    }
+
+    const exixtance = await Subscription.find({ channel: channelId });
+
+    if (!exixtance) {
+        throw new ApiError(400, "Invalid ChannalId");
+    }
+    //for subscriber get current userId who is going to subscribe a channel that is provided by params
+    //delete the document that user unsubscribed
+    //now in both case we are getting channelId now we have to toggle if user and channel in one doc then delete and if it's not then create it
+    try {
+
+        const toggle = await Subscription.findOne({
+            subscriber: req.user._id,
+            channel: channelId
+        });
+
+        if (toggle) {
+            await Subscription.deleteOne({
+                subscriber: req.user,
+                channel: channelId
+            })
+            return res.status(200).json(
+                new ApiResponse(200, toggle, "Unsusbscribed Successfully")
+            )
+        } else {
+            const newUser = await Subscription.create({
+                subscriber: req.user._id,
+                channel: channelId
+            })
+            return res.status(201).json(
+                new ApiResponse(201, newUser, "Subscribed Successfully")
+            )
+        }
+    } catch (error) {
+        throw new ApiError(
+            500,
+            "Error while toggling subscription",
+            [],
+            error?.stack
+        );
+    }
 })
 
 // controller to return subscriber list of a channel
